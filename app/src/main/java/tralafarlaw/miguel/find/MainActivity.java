49daa6d22633febc70215d2d4020f2c38e141032
Tralafarlaw <tralafarlaw@gmail.com>
2018-11-01 22:31:38 -0400

    login listo

--- app/src/main/java/tralafarlaw/miguel/find/MainActivity.java
+++ app/src/main/java/tralafarlaw/miguel/find/MainActivity.java
@@ -1,12 +1,16 @@
 package tralafarlaw.miguel.find;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -26,10 +30,34 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         mapiclient = new GoogleApiClient.Builder(getApplicationContext()).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
         SignInButton Login_Button = findViewById(R.id.btnSignIn);
+        Login_Button.setOnClickListener(this);
+
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent it = Auth.GoogleSignInApi.getSignInIntent(mapiclient);
+        startActivity(it);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        GoogleSignInResult result  = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+        TrabajarConElLogin(result);
+    }
+    public void TrabajarConElLogin (GoogleSignInResult result){
+        if(result.isSuccess()){
+            GoogleSignInAccount acc = result.getSignInAccount();
+            //trabajar con la cuenta acc que es el usuario
+        }
+    }
+
 }
