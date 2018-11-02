836ceafcbe7e384e80b45255b9579c9f6fabc56b
Tralafarlaw <tralafarlaw@gmail.com>
2018-11-02 18:44:55 -0400

    el mapa ya muestra la ubicacion del usuario solo falta mandarla al firbase y jalar a todas de ahi

--- app/src/main/java/tralafarlaw/miguel/find/MainActivity.java
+++ app/src/main/java/tralafarlaw/miguel/find/MainActivity.java
@@ -1,32 +1,15 @@
 package tralafarlaw.miguel.find;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class MainActivity extends AppCompatActivity{
@@ -71,7 +54,7 @@ public class MainActivity extends AppCompatActivity{
     private void startNewActivity(int resultCode, Intent data) {
         if(resultCode == RESULT_OK)
         {
-            Intent intent = new Intent(MainActivity.this,mapaosm.class);
+            Intent intent = new Intent(getApplicationContext(),mapaosm.class);
             startActivity(intent);
             finish();
         }
@@ -117,7 +100,7 @@ public class MainActivity extends AppCompatActivity{
         Toast.makeText(getApplicationContext(), "login inicia", Toast.LENGTH_SHORT);
 
     }
-
+/*
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
--- app/src/main/java/tralafarlaw/miguel/find/mapaosm.java
+++ app/src/main/java/tralafarlaw/miguel/find/mapaosm.java
@@ -1,13 +1,88 @@
 package tralafarlaw.miguel.find;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
 public class mapaosm extends AppCompatActivity {
+
+    LocationManager locationManager;
+    MapView map;
+    Context ctx;
+    IMapController mapDriver;
+    Location yo;
+    public void setLocation(Location loc){
+        this.yo = loc;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         setContentView(R.layout.activity_mapaosm);
+        TextView tv =(TextView) findViewById(R.id.Nombre);
+//        tv.setText(getIntent().getExtras().getString("Nombre"));
+
+        map = (MapView) findViewById(R.id.mapaOSM);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        mapDriver = map.getController();
+
+
+        ActivityCompat.requestPermissions(mapaosm.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(getApplicationContext(),"No se dieron Permisos",Toast.LENGTH_SHORT).show();
+        }
+
+
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        MyLocationListener mlocListener = new MyLocationListener();
+        mlocListener.setMainActivity(this);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,(LocationListener) mlocListener);
+
+        //locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        yo = mlocManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+
+        Toast.makeText(getApplicationContext(),""+String.valueOf(yo.getLatitude())+" "+String.valueOf(yo.getLongitude()), Toast.LENGTH_SHORT).show();
+
+        GeoPoint starPoint = new GeoPoint(yo.getLatitude(),yo.getLongitude());
+
+
+
+
+        mapDriver.setCenter(starPoint);
+        mapDriver.setZoom(9.0);
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
