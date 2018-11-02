6ec2dd2b2fab0775cd4d7848f1ecae526b40d39a
Tralafarlaw <tralafarlaw@gmail.com>
2018-11-01 21:18:50 -0400

    se creo la funcion inicio de sesion y se agrego la dependencia de google services

--- app/src/main/java/tralafarlaw/miguel/find/MainActivity.java
+++ app/src/main/java/tralafarlaw/miguel/find/MainActivity.java
@@ -1,13 +1,32 @@
 package tralafarlaw.miguel.find;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    //Cliente del API de firebase
+    GoogleApiClient mapiclient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void inicio_de_sesion(){
+        //creamos el menu para seleccionar el correo que jalara del celular
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        mapiclient = new GoogleApiClient.Builder(getApplicationContext()).enableAutoManage()
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
---
+++ app/src/main/java/tralafarlaw/miguel/find/mapaosm.java
@@ -0,0 +1,13 @@
+package tralafarlaw.miguel.find;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mapaosm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapaosm);
+    }
+}
