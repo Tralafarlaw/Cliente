547a5999e8d468af8dcae6380bc900c12fee18f5
Tralafarlaw <tralafarlaw@gmail.com>
2018-11-01 22:12:48 -0400

    se creo la funcion inicio de sesion y se agrego la dependencia de google services

--- app/src/main/java/tralafarlaw/miguel/find/MainActivity.java
+++ app/src/main/java/tralafarlaw/miguel/find/MainActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
@@ -22,7 +24,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     public void inicio_de_sesion(){
         //creamos el menu para seleccionar el correo que jalara del celular
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        mapiclient = new GoogleApiClient.Builder(getApplicationContext()).enableAutoManage()
+        mapiclient = new GoogleApiClient.Builder(getApplicationContext()).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+        SignInButton Login_Button = findViewById(R.id.btnSignIn);
     }
 
     @Override
