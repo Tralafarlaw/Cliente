dce1a08245c4239605fda3708196d1863eed06d1
Tralafarlaw <tralafarlaw@gmail.com>
2018-11-02 02:14:35 -0400

    algo anda mal con el login ya selecciona de los cooreos del celular pero no inician sesion en el firebase y no se como jalar las ubicaciones de los usuarios, podemos crear un metodo que jale las ubicaciones de todos luego agregarlas en un inflate como puntos de ubicacion y adicionar a estos el del dispositivo y con ayuda de google services volver la app un servicio para que funcione en segundo plano mandando y recibiendo la ubicacion en todo momento

--- app/src/main/java/tralafarlaw/miguel/find/MainActivity.java
+++ app/src/main/java/tralafarlaw/miguel/find/MainActivity.java
@@ -2,8 +2,10 @@ package tralafarlaw.miguel.find;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.auth.api.Auth;
@@ -14,11 +16,30 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
+    // firebase auth
+    FirebaseAuth mAuth;
 
     //Cliente del API de firebase
     GoogleApiClient mapiclient;
+
+    public static final String TAG = "SignInActivity";
+    public static final int RC_SIGN_IN = 9001;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapiclient.connect();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +48,48 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
         
         setContentView(R.layout.activity_main);
+        inicio_de_sesion();
     }
     public void inicio_de_sesion(){
         //creamos el menu para seleccionar el correo que jalara del celular
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        mapiclient = new GoogleApiClient.Builder(getApplicationContext()).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mapiclient = new GoogleApiClient.Builder(getApplicationContext())
+                .enableAutoManage(this, this)
+               // .addConnectionCallbacks(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .addApi(LocationServices.API)
+                .build();
         SignInButton Login_Button = findViewById(R.id.btnSignIn);
         Login_Button.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
 
+    }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "login exitoso");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "error de login", task.getException());
+                           // Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     @Override
@@ -60,7 +115,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         if(result.isSuccess()){
             GoogleSignInAccount acc = result.getSignInAccount();
             //trabajar con la cuenta acc que es el usuario
-
+            firebaseAuthWithGoogle(acc);
         }
     }
 
